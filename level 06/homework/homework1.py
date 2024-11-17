e7fefbcfc192654728d04b580f9cb1395a3ad221
deme-12 <mamuladzedeme5@gmail.com>
2024-11-17 14:52:04 +0100

    commit

--- level 06/homework/homework1.py
+++ level 06/homework/homework1.py
@@ -1,5 +1,5 @@
 # 1) შექმენი ორი input შეადარე ორი რიცხვი და დაბეჭდე შედეგი.
 
-num1 = input("enter number: ")
-num2 = input("enter number: ")
+num1 = int(input("enter number: "))
+num2 = int(input("enter number: "))
 print(num1 == num2)
