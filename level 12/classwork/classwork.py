ee02269e36024e003ce51420b5d52abad0c4b6fa
deme-12 <mamuladzedeme5@gmail.com>
2024-11-20 17:16:24 +0100

    commit

---
+++ level 12/classwork/classwork.py
@@ -0,0 +1,17 @@
+# for Loop
+
+# Loop - უსასრულოდ რომ მეორდება რაღაც  (უსასრულობა)
+# ციკლი - რამდენჯერმე რომ განმეორდება რაღაც  
+
+# iteration - იტერაცია - განმეორება 
+
+# for i in range(0, 100, 3):
+#     print(i)
+
+    
+for i in range(20):
+    if i % 2 == 0:
+        print(str(i) + "even")
+    else:
+        print(str(i) + "odd")
+    
