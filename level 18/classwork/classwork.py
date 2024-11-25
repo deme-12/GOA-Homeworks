afbf0cf7e9e2f4bd0fc4efbf03d7766498001037
deme-12 <mamuladzedeme5@gmail.com>
2024-11-25 20:08:44 +0100

    summit

---
+++ level 18/classwork/classwork.py
@@ -0,0 +1,6 @@
+# 1) მომხმარებელს შეეკითხეთ სახელი სანამ ეს სახელი არიქნება გაბრიელი მაქამდე არ გაჩერდეს და შეეკითხოს ისევ ხელახლა თუ იქნება გაბრიელი გაჩერდეს
+
+name = input("enter your name ")
+
+while name != "gabrieli":
+    name = input("please enter correct name ")
