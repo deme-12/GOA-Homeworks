a6a7c0c4fb1f93498f1e9f5a4716fb8f70aa084c
deme-12 <mamuladzedeme5@gmail.com>
2024-11-30 20:47:05 +0100

    commit

--- level 22/homework/homework01.py
+++ level 22/homework/homework01.py
@@ -6,16 +6,3 @@ def number(n):
 number(10)
 
     
-
-
-
-
-# 8 kyu
-# https://www.codewars.com/kata/523b4ff7adca849afe000035
-# https://www.codewars.com/kata/50654ddff44f800200000004
-# https://www.codewars.com/kata/55a2d7ebe362935a210000b2
-# https://www.codewars.com/kata/5168bb5dfe9a00b126000018
-# https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
-
-# 7 kyu
-# https://www.codewars.com/kata/546e2562b03326a88e000020
