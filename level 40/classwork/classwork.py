0f49ccce06253760bf9083b4cf6be6178bf8807e
deme-12 <mamuladzedeme5@gmail.com>
2024-12-12 20:18:07 +0100

    commit

--- level 40/classwork/classwork.py
+++ level 40/classwork/classwork.py
@@ -1,4 +1,36 @@
-https://www.codewars.com/kata/5959ec605595565f5c00002b
+
+def reverse_bits(n):
+    binary = bin(n)[2:]
+    str_bin = binary[::-1]
+
+    result = 0
+    for i in range(len(str_bin)):
+        result += int(str_bin[i]) * (2 ** (len(str_bin) - (i +1)))
+    return result
 
 
-https://www.codewars.com/kata/6071ef9cbe6ec400228d9531
+
+def calculator(txt):
+    str_list = txt.split()
+
+    s1 = len(str_list[0])
+    s2 = len(str_list[2])
+
+    if str_list[1] == "+": return (s1 + s2) * "."
+    elif str_list[1] == "-": return (s1 - s2) * "."
+    elif str_list[1] == "*": return (s1 * s2) * "."
+
+    return (s1 // s2) * "."
+
+
+
+
+def move_zeros(lst):
+    result = []
+    zeros = []
+    for num in lst:
+        if num != 0: result.append(num)
+        else: zeros.append(num)
+    return result + zeros
+
+
---
+++ level 40/homework/homework01.py
@@ -0,0 +1,21 @@
+# Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+# For example: (Input --> Output)
+
+# 10 --> 1
+# 99 --> 18
+# -32 --> 5
+# Let's assume that all numbers in the input will be integer values.
+
+
+
+def sum_digits(number):
+    number = str(abs(number))
+    # abs - ფუნქციის გამოყენებით თუ სიაში იქნება უარყოფითი რიცვები გადმოვიყვანთ დადებითში
+    sum = 0
+    for num in number:
+        sum += int(num)
+    return sum
+
+
+
